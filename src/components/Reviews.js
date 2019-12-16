028d12bb9cd1e62bd7729b28fc52f61f91759362
rubenperezf <rubenperezfeijoo@gmail.com>
2019-12-16 15:15:53 -0500

    3 step reviews

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import { Link } from "react-router-dom"
 import axios from 'axios';
+import { startSession } from 'mongoose';
+import OneStar from "../images/reviews-1star.png";
+import TwoStars from "../images/reviews-2stars.png";
+import ThreeStars from "../images/reviews-3stars.png";
+import FourStars from "../images/reviews-4stars.png";
+import FiveStars from "../images/reviews-5stars.png";
+
+function reviewStarts(rating) {
+    if(rating ===1) {
+        return <img src={OneStar} alt="rating"/>
+    } else if(rating ===2) {
+      return <img src={TwoStars} alt="rating"/>
+  }
+  else if(rating ===3) {
+      return <img src={ThreeStars} alt="rating"/>
+  } else if(rating ===4) {
+      return <img src={FourStars} alt="rating"/>
+  } else {
+      return <img src={FiveStars} alt="rating"/>
+  }
+}
 
 export default class Reviews extends React.Component {
   state = {
@@ -15,6 +36,7 @@ export default class Reviews extends React.Component {
       })
   }
 
+
   render() {
     return (
       <div className="reviews-container">
@@ -22,7 +44,7 @@ export default class Reviews extends React.Component {
             <legend key={this.state.reviews._id}>Comments</legend>
                 {this.state.reviews.map(review => 
                 <div className="each-review-container">
-                    
+                    <p>{reviewStarts(review.rating)}</p>
                     <p>Review: {review.review}</p>
                     <p>by: <Link className="link-each-review-container" to={`/family/${review.familyId}`}>{review.familyName}</Link></p>
                 </div>
