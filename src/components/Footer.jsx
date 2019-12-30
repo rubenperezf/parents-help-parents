fbd20e24cb0e7d799a03ef5407d074dad0263722
rubenperezf <rubenperezfeijoo@gmail.com>
2019-12-30 09:43:31 -0500

    social media with links to real pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,10 +12,10 @@ function Footer() {
       {/* <a href="https://www.instagram.com/parentshelpparents6/" target="_black"><img src={logoInstagram} alt="insagram logo"/></a>
       <a href="https://twitter.com/ParentsHelpPar1" target="_black"><img src={logoTwitter} alt="twitter logo"/></a> */}
         <div class="rounded-social-buttons">
-          <a class="social-button facebook" href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"><img src={logoFacebook}/></i></a>
-          <a class="social-button twitter" href={"https://www.twitter.com/"} target="_blank"><i class="fab fa-twitter"><img src={logoTwitter}/></i></a>
-          <a class="social-button youtube" href="https://www.youtube.com/" target="_blank"><i class="fab fa-youtube"><img src={logoYoutube}/></i></a>
-          <a class="social-button instagram" href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"><img src={logoInstagram}/></i></a>
+          <a class="social-button facebook" href="https://www.facebook.com/Parents-Help-Parents-106535904200289/?modal=admin_todo_tour" target="_blank"><i class="fab fa-facebook-f"><img src={logoFacebook}/></i></a>
+          <a class="social-button twitter" href="https://twitter.com/ParentsHelpPar1" target="_blank"><i class="fab fa-twitter"><img src={logoTwitter}/></i></a>
+          <a class="social-button youtube" href="https://www.youtube.com/channel/UCyZffBk77yPwlBT2FxLA2Kw?view_as=subscriber" target="_blank"><i class="fab fa-youtube"><img src={logoYoutube}/></i></a>
+          <a class="social-button instagram" href="https://www.instagram.com/parentshelpparents6/" target="_blank"><i class="fab fa-instagram"><img src={logoInstagram}/></i></a>
         </div>
         <p>&copy; Web Page made by Ruben Perez</p>
     </div>
