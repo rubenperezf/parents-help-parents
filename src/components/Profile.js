084de933c545edcfd35e2d1e06ad2cd8ab95adcf
rubenperezf <rubenperezfeijoo@gmail.com>
2019-12-31 08:51:14 -0500

    profile with create family

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { useAuth0 } from "../react-auth0-spa";
+import CreateFamily from "./CreateFamily";
 
 const Profile = () => {
   const { loading, user } = useAuth0();
@@ -10,11 +11,21 @@ const Profile = () => {
 
   return (
     <Fragment>
-    <div className="profile">
-      <img className="profile-picture"src={user.picture} alt="Profile" />
-      <h2>{user.name}</h2>
-      <p>{user.email}</p>
-    </div>
+
+      <div className="profile-container">
+
+        <div className="profile">
+        <fieldset>
+        <legend>Profile</legend>
+          <img className="profile-picture" src={user.picture} alt="Profile" />
+          <p>Username: {user.name}</p>
+          <p>Email: {user.email}</p>
+          </fieldset>
+        </div>
+    
+        <CreateFamily />
+      </div>
+  
     </Fragment>
   );
 };
