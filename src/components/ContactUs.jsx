d48247553f8431a60921a283e52725c98e1c49a6
rubenperezf <rubenperezfeijoo@gmail.com>
2020-01-02 15:41:31 -0500

    contact us working

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,16 +1,32 @@
 import React,{useState} from "react";
+import axios from "axios"
 
 function ContactUs() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [message, setMessate] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handlePost = (
+    name,
+    email,
+    message
+  ) => {
+    axios.post("http://localhost:2500/contactus", {
+      name: name,
+      email: email,
+      message: message
+    })
+  }
   return (
     <div className="contactUs-aboutUs-container form-contact-us">
       <h1>Contact Us</h1>
-      <form>
-        <input type="text"  placeholder="Your Name" />
-        <input type="email"  placeholder="Email Address" />
-        <textarea placeholder="Type your Message" rows="20" cols="1000"></textarea>
+      <form onSubmit= {e => {
+        e.preventDefault();
+        handlePost( name, email, message);
+      }}>
+        <input type="text"  placeholder="Your Name" onChange={ e => setName(e.target.value)} />
+        <input type="email"  placeholder="Email Address" onChange={ e => setEmail(e.target.value)}/>
+        <textarea placeholder="Type your Message" rows="20" cols="1000" onChange={ e => setMessage(e.target.value)}></textarea>
         <button className="button"><span>Send</span></button>
       </form>
     </div>
