c83f1b5684618aec58be22b9424a7c4ac43a026d
rubenperezf <rubenperezfeijoo@gmail.com>
2020-01-04 10:33:27 -0500

    filter age working

--- src/components/Families.jsx
+++ src/components/Families.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect } from "react";
+import React, { useReducer, useEffect,useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import FamilyFilter from "./FamilyFilter"
@@ -18,6 +18,7 @@ const initialData = {
 };
 function Families() {
   const [data, dispatch] = useReducer(dataReducer, initialData);
+  const [filterValue, setFilterValue] = useState('')
   useEffect(() => {
     axios
       .get("http://localhost:2500/families")
@@ -29,45 +30,51 @@ function Families() {
         dispatch({ type: "SET_ERROR" });
       });
   }, []);
+  const callbackFilterAge = val => {
+    setFilterValue(val);
+  };
+  console.log(filterValue)
+
   return (
     <div> 
-      <FamilyFilter />
+      <FamilyFilter parentCallback={callbackFilterAge}/>
     <div className="display-families-container">
-      {data.list.map(family => {
-        return (
-          <div className="row" key={family._id}>
-            <div className="col-12 col-sm-6 col-md-4 col-lg-3">
-              <div className="our-team">
-                <div>
-                  <div className="picture">
-                    <img
-                      className="img-fluid"
-                      src={family.images[0]}
-                      alt="family together"
-                    />
-                  </div>
-                  <div className="team-content">
-                    <h3 className="name">Family: {family.familyName}</h3>
-                    <p className="title">Parents names: {family.parentsName}</p>
-                    <p className="title">
-                      Number of kids: {family.numberOfKids}
-                    </p>
-                    <p className="title">
-                      Kids Age: {family.kidsAge.map(element => element + " ")}
-                    </p>
-                    <div className="social">
-                      <Link to={`/family/${family._id}`}>See more</Link>
+    {data.list.filter(families => 
+            families.numberOfKids   === filterValue )
+              .map(family => {
+                return (
+                  <div className="row" key={family._id}>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+                      <div className="our-team">
+                        <div>
+                          <div className="picture">
+                            <img
+                              className="img-fluid"
+                              src={family.images[0]}
+                              alt="family together"
+                            />
+                          </div>
+                          <div className="team-content">
+                            <h3 className="name">Family: {family.familyName}</h3>
+                            <p className="title">Parents names: {family.parentsName}</p>
+                            <p className="title">
+                              Number of kids: {family.numberOfKids}
+                            </p>
+                            <p className="title">
+                              Kids Age: {family.kidsAge.map(element => element + " ")}
+                            </p>
+                            <div className="social">
+                              <Link to={`/family/${family._id}`}>See more</Link>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          
-        );
-        
-      })}
-     
+                  
+                );
+              })}
+      
     </div>
     </div>
   );
--- src/components/FamilyFilter.js
+++ src/components/FamilyFilter.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-function FamilyFilter() {
+function FamilyFilter({...props}) {
+    const [filter, setFilter] = React.useState('');
+    const handleChange = e => {
+        setFilter(e.target.value);
+        props.parentCallback(e.target.value)
+      };
   return (
     <div className="family-filter-container">
       <div className="family-filter">
@@ -10,7 +15,7 @@ function FamilyFilter() {
             <label htmlFor="filter-families-age">Age:</label>
           </div>
           <div className="filter-options">
-            <select id="filter-families-age">
+            <select onChange={handleChange} id="filter-families-age">
               <option> </option>
               <option>1</option>
               <option>2</option>
@@ -20,25 +25,25 @@ function FamilyFilter() {
             </select>
           </div>
         </div>
-        <div className="filter-location">
-        <div className="filter-text">
-          <label htmlFor="filter-families-location">Location:</label>
+        {/* <div className="filter-location">
+          <div className="filter-text">
+            <label htmlFor="filter-families-location">Location:</label>
           </div>
           <div className="filter-options">
-          <select id="filter-families-location">
-            <option> </option>
-            <option>Mt. Pleasant</option>
-            <option>James Island</option>
-            <option>North Charleston</option>
-            <option>Island of Palms</option>
-            <option>Folly Beach</option>
-            <option>Edisto Island</option>
-            <option>Goose Creek</option>
-            <option>Kiawah Island</option>
-            <option>Sullivans Island</option>
-          </select>
+            <select id="filter-families-location">
+              <option> </option>
+              <option>Mt. Pleasant</option>
+              <option>James Island</option>
+              <option>North Charleston</option>
+              <option>Island of Palms</option>
+              <option>Folly Beach</option>
+              <option>Edisto Island</option>
+              <option>Goose Creek</option>
+              <option>Kiawah Island</option>
+              <option>Sullivans Island</option>
+            </select>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
