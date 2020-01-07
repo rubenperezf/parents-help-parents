8ae6adfdbcb6672f7a3e638d1dc689b03c286e06
rubenperezf <rubenperezfeijoo@gmail.com>
2020-01-07 11:15:20 -0500

    delete working well

--- src/components/DeleteFamily.js
+++ src/components/DeleteFamily.js
@@ -2,7 +2,6 @@ import React, { useReducer, useEffect, useState } from "react";
 import axios from "axios";
 import { useAuth0 } from "../react-auth0-spa";
 
-
 export const dataReducer = (state, action) => {
   if (action.type === "SET_ERROR") {
     return { ...state, list: [], error: true };
@@ -17,36 +16,41 @@ const initialData = {
   error: null
 };
 function DeleteFamily() {
-    const [data, dispatch] = useReducer(dataReducer, initialData);
-    const { loading, user } = useAuth0();
-  
-    useEffect(() => {
-      axios
-        .get("http://localhost:2500/families")
-        .then(response => {
-          console.log(response);
-          dispatch({ type: "SET_LIST", list: response.data });
-        })
-        .catch(() => {
-          dispatch({ type: "SET_ERROR" });
-        });
-    }, []);
-    const handleDelete = id => {
-        axios.delete(`http://localhost:2500/family/${id}`);
-      };
-    return (
-        <div>
-                    {data.list
-          .filter(element => element.userName === user.name)
-          .map(family => {
-              return (
-                <button key={family._id} onSubmit={handleDelete(family._id)} className="button"><span>Delete Family</span></button>
-              )
-          })}
-           
-        </div>
-        
-    )
+  const [data, dispatch] = useReducer(dataReducer, initialData);
+  const { loading, user } = useAuth0();
+    
+  useEffect(() => {
+    axios
+      .get("http://localhost:2500/families")
+      .then(response => {
+        console.log(response);
+        dispatch({ type: "SET_LIST", list: response.data });
+      })
+      .catch(() => {
+        dispatch({ type: "SET_ERROR" });
+      });
+  }, []);
+  const handleDelete = id => {
+    console.log("handleDelete")
+    axios.delete(`http://localhost:2500/family/${id}`);
+  };
+  return (
+    <div>
+      {data.list
+        .filter(element => element.userName === user.name)
+        .map(family => {
+          return (
+            <button
+              key={family._id}
+              onClick={()=> handleDelete(family._id)}
+              className="button"
+            >
+              <span>Delete Family</span>
+            </button>
+          );
+        })}
+    </div>
+  );
 }
 
-export default DeleteFamily
+export default DeleteFamily;
