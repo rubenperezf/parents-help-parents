f2812e3cf62808afebdf1cb157dc0476ba911019
rubenperezf <rubenperezfeijoo@gmail.com>
2020-01-21 16:45:31 -0500

    about us completed

--- src/components/Header/AboutUs.jsx
+++ src/components/Header/AboutUs.jsx
@@ -9,6 +9,10 @@ function AboutUs() {
       connect with other families that share similar values. We facilitate 
       new relationships that allow families to collaborate with childcare when needed.
       </p>
+      <p>
+      Parents Help Parents was founded on the need for new parents to find a support system
+      with people they can trust who are also going through similar experiences. 
+      </p>
     </div>
   );
 }
